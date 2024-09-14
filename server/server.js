97a894e9c63eb7eee911a20a68c46adb9c43c0ac
mendelorian <ipanda303@gmail.com>
2024-09-14 16:38:23 +0800

    Change default port to something more commonly used - 3000

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import blogRoutes from './routes/blogRoutes.js';
 // Load environment variables
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 connectDB();
