a83123ff1365a1bcf0976fccbe1cec68ebb00dc5
mendelorian <ipanda303@gmail.com>
2024-09-24 15:22:58 +0800

    Completed blogForm component

---
+++ client/src/components/BlogForm.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const BlogForm = ({ onCreate }) => {
+  const [form, setForm] = useState({ topic: '', author: ''});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await onCreate(form);
+      setForm({ topic: '', author: ''});
+    } catch (err) {
+      console.error('Error creating blog: ', err);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Blog Topic"
+        value={form.topic}
+        onChange={(e) => setForm({ ...form, topic: e.target.value })}
+        required
+      />
+      <input
+        type="text"
+        placeholder="Author"
+        value={form.author}
+        onChange={(e) => setForm({ ...form, author: e.target.value })}
+        required
+      />
+      <button type="submit">Generate Blog</button>
+    </form>
+  )
+}
+
+export default BlogForm;
